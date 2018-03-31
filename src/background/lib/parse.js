cc72a511d330fdb6231b271e56a5c046989aead4
yan vologzhanin <yanvolo@gmail.com>
2018-03-31 16:51:36 -0400

    added parse method

---
+++ src/background/lib/parse.js
@@ -0,0 +1,66 @@
+/* Global Variables */
+
+var category = ["SAFE","QUESTIONABLE","UNSAFE"];
+Object.freeze(category);
+
+//@TODO: Is this safe to put the key here?
+var key = "AIzaSyD0CBXaJ7trnGq8iD1qEzgpoRNajGnneIs";
+var url = "https://commentanalyzer.googleapis.com/v1alpha1/comments:analyze?key="  + key;
+/** Parses a message through the fishie score and returns HTML to print in report. 
+* @param - message - A string that refers to the message text of the tweet.
+* @return HTMLRet - The full HTML of the response, with a leading term from category.
+*/
+function parseThroughPerspective(message){
+    var queryPromise = new Promise(function (resolve, reject){
+        /*
+            Perspective API Section
+        */
+        //Build Request
+        var xhr = new XMLHttpRequest();   // new HttpRequest instance 
+        xhr.open("POST", url, true);
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.onload = (response) => {
+            var myData = JSON.parse(response.target.response);
+            resolve(myData)
+        }
+        xhr.onerror = reject;
+
+        //Generate Request for Perspective API
+        var request = {};
+        request.comment = {};
+        request.comment.text = message;
+        request.languages = ["en"];
+        request.requestedAttributes = {TOXICITY_FAST:{}};
+        request = JSON.stringify(request);
+
+        xhr.send(request);        
+    });
+    
+    
+    return queryPromise.then(function(ret){
+            
+            let div = document.createElement("div");
+            let scoreArray = ret.attributeScores.TOXICITY_FAST.spanScores; 
+            for(let i=0;i<scoreArray.length;i++){
+                let currentSpan = document.createElement("span");
+                currentSpan.innerHTML = message.substring(scoreArray[i].begin, scoreArray[i].end);
+                if(scoreArray[i].score.value > 0.5){
+                    currentSpan.setAttribute("style","text-decoration: underline;");    
+                }
+                div.appendChild(currentSpan);    
+            }
+            return div;
+            
+        },);
+
+    
+    
+    
+    /*
+        Botson Section
+    */
+    
+    /*
+        Skim Tweets and test on Opensources.co
+    */
+}
